2c9d3821817b9ac3929f090e4a0c0e98f4c81984
Dai Jun Tang <tang.ddt@gmail.com>
2011-02-22 19:32:26 -0800

    Restructure - with Jss prototype

--- jss.js
+++ jss.js
@@ -7,147 +7,153 @@
  */
 
 var jss = (function (doc, undefined) {
-    var jss,
-        head,
-        styleSheets;
-
-
-    // Shortcuts
-    head = doc.head || doc.getElementsByTagName('head')[0];
-    styleSheets = doc.styleSheets;
+    var Jss,
+        jss,
+        _addRule,
+        _addSheet,
+        // Shortcuts
+        head = doc.head || doc.getElementsByTagName('head')[0],
+        styleSheets = doc.styleSheets;
     
-    jss = function (selector, props) {
-        var modified = 0,
+    Jss = function (selector) {
+        var sheet,
             rules,
-            i,
-            j,
-            propName;
-
-        if (props) {
-            // Sets properties on the rule
-            if (jss.styleSheet) {
-                modified = jss.get(selector, jss.styleSheet, 'set', props).length;
-            }
-            if (!modified) {
-                jss.create(selector);
-                jss.get(selector, jss.styleSheet, 'set', props);
-            }
-            return;
-        } else {
-            // Returns static, consolidated map of properties
-            rules = jss.get(selector);
-            props = {};
-            for (i = 0; i < rules.length; i++) {
-                for (j = 0; j < rules[i].style.length; j++) {
-                    propName = rules[i].style[j];
-                    props[propName] = rules[i].style[propName];
-                }
-            }
-            return props;
-        }
-    };
-    
-    jss.get = function (selector, targetSheet, action /*, actionArgs */) {
-        if (!styleSheets) return [];
-        
-        var sheets = styleSheets,
-            sheet,
-            rules,
-            result = [],
-            actionRet,
-            actionArgs,
-            argsStart = 3,
+            rule,
             i,
             j;
         
-        // Normalise args
-        if (typeof targetSheet == 'string') {
-            action = targetSheet;
-            argsStart--;
-        } else if (targetSheet !== undefined) {
-            sheets = [targetSheet];
-        }
-        actionArgs = Array.prototype.slice.call(arguments, argsStart);
-        selector = selector.toLowerCase();
-
-        for (i = 0; i < sheets.length; i++) {
-            // Get rules for stylesheet, continue if not available
-            sheet = sheets[i];
+        this.rules = [];
+        
+        for (i = 0; i < styleSheets.length; i++) {
+            sheet = styleSheets[i];
             rules = sheet.cssRules || sheet.rules;
-            if (!rules) continue;
 
             for (j = 0; j < rules.length; j++) {
-                if (rules[j].selectorText.toLowerCase() == selector) {
-                    if (action) {
-                        actionRet = jss.actions[action]({
-                            args: actionArgs,
-                            sheet: sheet,
-                            rule: rules[j],
-                            rulePos: j
-                        });
-                        result.push(actionRet === undefined ?
-                            rules[j] : actionRet);
-                    } else {
-                        result.push(rules[j]);
-                    }
+                rule = rules[j];
+                // Warning, selectorText may not be correct in IE<9
+                // as it splits ',' into multiple selectors
+                if (rule.selectorText.toLowerCase() == selector) {
+                    // IE<9 support
+                    if (!rule.parentStyleSheet) rule.parentStyleSheet = sheet;
+                    this.rules.push(rule);
                 }
             }
         }
-
-        return result;
+        
+        this.selector = selector;
+        this.length = this.rules.length;
     };
-
-    jss.actions = {
-        remove: function (o) {
-            var sheet = o.sheet,
-                pos = o.rulePos;
-
-            if (sheet.deleteRule) {
-                sheet.deleteRule(pos);
-            } else if (sheet.removeRule) {
-                sheet.removeRule(pos);
+    
+    Jss.prototype = {
+        get: function () {
+            // Returns static, consolidated map of properties
+            var props = {},
+                propName,
+                i,
+                j;
+            for (i = 0; i < this.rules.length; i++) {
+                for (j = 0; j < this.rules[i].style.length; j++) {
+                    propName = this.rules[i].style[j];
+                    props[propName] = this.rules[i].style[propName];
+                }
             }
+            return props;
         },
-        set: function (o) {
-            var rule = o.rule,
-                props = o.args[0];
-
-            if (!props) return;
-            for (var i in props) {
+        set: function (props) {
+            if (!props) return this;
+            
+            var styleNode,
+                i,
+                rule;
+            if (!jss.styleSheet) jss.styleSheet = _addSheet('jss');
+            // Find if there's a rule in the jss stylesheet
+            for (i = 0; i < this.length; i++) {
+                if (this.rules[i].parentStyleSheet === jss.styleSheet) {
+                    rule = this.rules[i];
+                    break;
+                }
+            }
+            if (!rule) {
+                // Add rule
+                rule = _addRule(jss.styleSheet, this.selector);
+                this.rules.push(rule);
+                this.length = this.rules.length;
+            }
+            // Add props
+            for (i in props) {
                 if (!props.hasOwnProperty(i)) continue;
                 rule.style[i] = props[i];
             }
+            return this;
+        },
+        remove: function () {
+            var rule,
+                parentSheet,
+                rules,
+                i,
+                j;
+            for (i = 0; i < this.rules.length; i++) {
+                rule = this.rules[i];
+                parentSheet = rule.parentStyleSheet;
+                rules = parentSheet.cssRules || parentSheet.rules;
+                for (j = 0; j < rules.length; j++) {
+                    if (rules[j] === rule) {
+                        if (parentSheet.deleteRule) {
+                            parentSheet.deleteRule(j);
+                        } else if (parentSheet.removeRule) {
+                            parentSheet.removeRule(j);
+                        }
+                    }
+                }
+            }
+            this.rules.length = 0;
+            this.length = 0;
+            return this;
         }
     };
     
-    jss.create = function (selector) {
-        if (!styleSheets) return;
+    jss = function (selector, props) {
+        var ret = new Jss(selector);
         
-        var styleNode,
-            i;
-
-        // Create own stylesheet for jss styles
-        if (!jss.styleSheet) {
-            styleNode = doc.createElement('style');
-            styleNode.type = 'text/css';
-            styleNode.rel = 'stylesheet';
-            styleNode.media = 'screen';
-            styleNode.title = 'jss';
-            head.appendChild(styleNode);
-            // Find stylesheet object
-            for (i = 0; i < styleSheets.length; i++) {
-                if (styleNode === (styleSheets[i].ownerNode
-                        || styleSheets[i].owningElement)) {
-                    jss.styleSheet = styleSheets[i];
-                }
-            }
+        if (props) {
+            ret.set(props);
         }
         
+        return ret;
+    };
+    
+    jss.fn = Jss.prototype;
+    
+    _addRule = function (sheet, selector) {
         // Add (empty) rule
-        if (jss.styleSheet.insertRule) {
-            jss.styleSheet.insertRule(selector + ' { }', 0);
-        } else if (jss.styleSheet.addRule) {
-            jss.styleSheet.addRule(selector, null, 0);
+        if (sheet.insertRule) {
+            sheet.insertRule(selector + ' { }', 0);
+        } else if (sheet.addRule) {
+            sheet.addRule(selector, null, 0);
+        }
+        // Get added rule
+        if (sheet.cssRules) {
+            return sheet.cssRules[0];
+        } else {
+            return sheet.rules[0];
+        }
+    };
+    
+    _addSheet = function (title) {
+        var styleNode,
+            i;
+        styleNode = doc.createElement('style');
+        styleNode.type = 'text/css';
+        styleNode.rel = 'stylesheet';
+        styleNode.media = 'screen';
+        styleNode.title = title;
+        head.appendChild(styleNode);
+        // Get added stylesheet object
+        for (i = 0; i < styleSheets.length; i++) {
+            if (styleNode === (styleSheets[i].ownerNode
+                    || styleSheets[i].owningElement)) {
+                return styleSheets[i];
+            }
         }
     };
 
